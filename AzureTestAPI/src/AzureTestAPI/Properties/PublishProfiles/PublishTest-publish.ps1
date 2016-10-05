3861a65ac295fbf82a90a38fed63a23ca02cab9a
rbasw-eblain <eli.blain@quisitive.com>
2016-10-05 15:32:47 -0500

    Added dotnet core unit test to repo for build and test.

---
+++ AzureTestAPI/src/AzureTestAPI/Properties/PublishProfiles/PublishTest-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'eae98d50-a9ff-44df-84fe-93470ac353fd'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
---
+++ AzureTestAPI/test/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using AzureTestAPI.Controllers;
+using System.Collections.Generic;
+
+namespace AzureTestAPI.Tests
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void Get_Test() 
+        {
+            ValuesController controller = new ValuesController();
+            var responseContent = new List<string>();
+            foreach(string s in controller.Get())
+            {
+                responseContent.Add(s);
+            }
+
+            if (responseContent.Count > 0)
+                Assert.True(true);
+            else
+                Assert.True(false);
+        }
+    }
+}
